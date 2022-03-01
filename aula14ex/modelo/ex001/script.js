691ce6a347a8da66c91c7089fbdcb429b2b78c18
Arilton Almeida <97132920+ariltonalmeida@users.noreply.github.com>
2022-03-01 19:47:12 -0300

    criando exercícios novos

---
+++ aula14ex/modelo/ex001/script.js
@@ -0,0 +1,6 @@
+function contar(){
+    var inicio = document.getElementById('txtinicio') 
+    var fim = document.getElementById('txtfim')
+    var passo = document.getElementById('txtpasso')
+    var res = document.getElementById('div#res')
+}
