87cb72093a92f769e57b58c37e6a7de2dd0323af
Arilton Almeida <97132920+ariltonalmeida@users.noreply.github.com>
2022-03-08 18:44:07 -0300

    Completando exercício001 da aula 14

--- aula14ex/modelo/ex001/script.js
+++ aula14ex/modelo/ex001/script.js
@@ -1,8 +1,28 @@
 function contar(){
-    var inicio = document.getElementById('txtinicio') 
-    var fim = document.getElementById('txtfim')
-    var passo = document.getElementById('txtpasso')
-    var res = document.getElementById('div#res')
-    while (inicio <= fim)
-    var inicio = passo + inicio
+    var inicio = document.getElementById('txti') 
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var res = document.getElementById('res')
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'iMPOSSÍVEL CONTAR!'
+    } else {
+        res.innerHTML = 'Contando: '
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if (i < f) {
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+            
+        } else {
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`   
+    }
+    
 }
